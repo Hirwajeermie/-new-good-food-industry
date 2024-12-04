f6bc807d735393df2f363ec83639ee02a553817d
heshteo <118670695+heshteo@users.noreply.github.com>
2024-12-04 20:50:41 +0400

    Update functions.jsx

--- public/functions.jsx
+++ public/functions.jsx
@@ -1,7 +1,7 @@
 export async function f(url,options){
     try {
       Object.assign(options.headers,{'authorization': getdata('token') ? 'Bearer '+getdata('token') : null})
-      let z = await fetch('http://127.0.0.1:3001/' +url,options);
+      let z = await fetch('https://cfp.onrender.com' +url,options);
       let y = await z.json();
       return y;
     } catch (error) {
